e068c72fe262d2a9710d2094de18e03db33eb78e
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-21 22:06:33 +0300

    When you submit a file, you receive the file name, type, and size in bytes within the JSON response

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const multer = require("multer");
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const upload = multer({ dest: 'uploads/' })
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const upfile = req.file;
+  console.log(upfile);
+  res.json({ name: upfile.originalname, type: upfile.mimetype, size: upfile.size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
